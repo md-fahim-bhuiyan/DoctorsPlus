5c66123eddf8c49efa6bb90b6d59ac439ac941c3
Md. Fahim Bhuiyan <67233830+md-fahim-bhuiyan@users.noreply.github.com>
2023-03-19 01:22:33 +0600

    registaion update

--- Home/urls.py
+++ Home/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('home', views.home, name='home'),
+    path('doctor_registration', views.doctor_registration, name='doctor_registration'),
+    path('patient_registration', views.patient_registration, name='patient_registration'),
+
 ]
--- Home/views.py
+++ Home/views.py
@@ -1,5 +1,45 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from .forms import DoctorRegistrationForm, PatientRegistrationForm
+from .models import Doctor, Patient
+
+
 def home(request):
     return render(request, 'Home/index.html')
+
+
+def doctor_registration(request):
+    if request.method == 'POST':
+        form = DoctorRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            specialty = form.cleaned_data['specialty']
+            bio = form.cleaned_data['bio']
+            contact = form.cleaned_data['contact']
+            doctor = Doctor(user=user, specialty=specialty, bio=bio, contact=contact)
+            doctor.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = DoctorRegistrationForm()
+    return render(request, 'doctor_registration.html', {'form': form})
+
+def patient_registration(request):
+    if request.method == 'POST':
+        form = PatientRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            phone = form.cleaned_data['phone']
+            address = form.cleaned_data['address']
+            gender = form.cleaned_data['gender']
+            patient = Patient(user=user, phone=phone, address=address, gender=gender)
+            patient.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = PatientRegistrationForm()
+    return render(request, 'patient_registration.html', {'form': form})
