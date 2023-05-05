04f12437c9409181cb62bcbede7e94374025b644
Md. Fahim Bhuiyan <67233830+md-fahim-bhuiyan@users.noreply.github.com>
2023-05-05 09:58:03 +0600

    remove unnecessary code

--- Home/forms.py
+++ Home/forms.py
@@ -1,10 +1,1 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
-from .models import PatientRegister
-from django.forms import ModelForm
-
-class PatientRegister(ModelForm):
-    class Meta:
-        model = PatientRegister
-        fields = '__all__'
--- Home/urls.py
+++ Home/urls.py
@@ -4,8 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('home', views.home, name='home'),
-    # path('patientlogin', views.patientlogin, name='patientlogin'),
-    # path('patientregister', views.patientregister, name='patientregister'),
     path('accounts/profile/', views.profile, name='profile'),
     path('logout', views.logout, name='logout'),
 ]
