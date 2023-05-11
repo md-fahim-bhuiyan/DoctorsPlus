342f91d8aaa1979099f33eed33f598eb743366e5
Md. Fahim Bhuiyan <67233830+md-fahim-bhuiyan@users.noreply.github.com>
2023-05-11 22:31:10 +0600

    doner

--- patient/admin.py
+++ patient/admin.py
@@ -6,6 +6,6 @@ admin.site.register(Appointment)
 
 
 class DonationRequestAdmin(admin.ModelAdmin):
-    list_display = ('user', 'blood_group', 'units_required', 'location', 'contact_number', 'is_approved', 'created_at')
+    list_display = ('user','doner_name', 'blood_group', 'units_required', 'location', 'contact_number', 'is_approved', 'created_at')
 
 admin.site.register(DonationRequest, DonationRequestAdmin)
